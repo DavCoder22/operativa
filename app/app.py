7f679b98be81ffc72ba564c076af1139eec8a275
DavCoder22 <87149195+DavCoder22@users.noreply.github.com>
2024-08-01 21:25:09 -0500

    varsion final transporte

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template,request
-import re
+from flask import Flask, jsonify, render_template,request
+import numpy as np
 
+from transporte.TransporteSolver import solve_transportation_problem, vogel_approximation_method
 from lineal.LinearProgrammingSolver import LinearProgrammingSolver
 from gpt.GptAnaliser import GptAnaliser
 
@@ -12,7 +13,6 @@ def index():
 @app.route('/objective')
 def objetivo():
     return render_template('objetivo.html')
-
 
 @app.route('/linear', methods=['GET', 'POST'])
 def linear():
@@ -43,10 +43,46 @@ def linear():
 
     # Renderizar la plantilla con resultados
     return render_template('linear-programming.html')
-@app.route('/transportation')
-def transportation():
-    return render_template('transportation.html')
 
+
+@app.route('/transportation', methods=['GET', 'POST'])
+def transportation():
+    if request.method == 'POST':
+        try:
+            data = request.json
+            num_sources = int(data['numSources'])
+            num_destinations = int(data['numDestinations'])
+            supply = [int(data[f'supply{i}']) for i in range(num_sources)]
+            demand = [int(data[f'demand{j}']) for j in range(num_destinations)]
+            cost = [[int(data[f'cost{i}{j}']) for j in range(num_destinations)] for i in range(num_sources)]
+            cost_matrix = np.array(cost, dtype=float)
+
+            # Equilibrar el problema si es necesario
+            if sum(supply) != sum(demand):
+                if sum(supply) > sum(demand):
+                    demand.append(sum(supply) - sum(demand))
+                    cost_matrix = np.hstack((cost_matrix, np.zeros((cost_matrix.shape[0], 1))))
+                else:
+                    supply.append(sum(demand) - sum(supply))
+                    cost_matrix = np.vstack((cost_matrix, np.zeros((1, cost_matrix.shape[1]))))
+
+            vogel_allocation = vogel_approximation_method(supply, demand, cost_matrix.tolist())
+            vogel_cost = np.sum(vogel_allocation * cost_matrix)
+            simplex_allocation, simplex_cost = solve_transportation_problem(supply, demand, cost_matrix.tolist())
+            return jsonify({
+                'initial_matrix': cost_matrix.tolist(),
+                'vogel': {
+                    'allocation': vogel_allocation.tolist(),
+                    'cost': vogel_cost
+                },
+                'simplex': {
+                    'allocation': simplex_allocation,
+                    'cost': simplex_cost
+                }
+            })
+        except Exception as e:
+            return jsonify({'error': str(e)})
+    return render_template('transportation.html')
 
 
 if __name__ == '__main__':
